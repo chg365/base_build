b1bfe455b260f1cad8bdb25308eff2c8c80dfd74
Cui Haiguang <cuihaiguang@eyou.net>
2017-10-12 11:20:33 +0800

    mod

--- build/renew_cert.sh
+++ build/renew_cert.sh
@@ -5,6 +5,42 @@ DEHYDRATED_BASE=@DEHYDRATED_BASE@
 CONFIG_DIR=@DEHYDRATED_CONFIG_DIR@
 NGINX_BASE=@NGINX_BASE@
 
+# {{{ function delete_expire_cert_file() 删除过期的证书文件
+function delete_expire_cert_file() {
+    local CERTS_DIR=$CONFIG_DIR/certs
+
+    if [ ! -d "$CERTS_DIR" ];then
+        echo "证书目录不存在" >&2
+        exit 1;
+    fi
+
+    local link_files=(
+            "cert.csr"
+            "cert.pem"
+            "chain.pem"
+            "fullchain.pem"
+            "privkey.pem"
+            )
+
+    for domain_dir in `find $CERTS_DIR -mindepth 1 -maxdepth 1 -type d`; do
+        for((i=0; i<${#link_files[*]}; i++)); do
+            local file_name=${link_files[$i]}
+            local link_file="$domain_dir/$file_name"
+            if [ -h "$link_file" ];then
+                local real_file=`realpath $link_file`
+                local real_file_name="${real_file##*/}"
+                local f;
+                for f in `find $domain_dir -name "${file_name/./-*.}" -type f`; do
+                    if [ "${f##*/}" != "$real_file_name" ]; then
+                        rm -f ${f}
+                    fi
+                done
+            fi
+        done
+    done
+}
+# }}}
+
 if [ ! -d "$CONFIG_DIR/accounts" ] || ! test -f `find $CONFIG_DIR/accounts/ -name account_key.pem` ; then
     $DEHYDRATED_BASE/sbin/dehydrated --register --accept-terms -f $CONFIG_DIR/config
     flag1=$?
@@ -36,6 +72,8 @@ if  which systemctl >/dev/null 2>&1 && [ -f /usr/lib/systemd/system/${project}.n
 else
     $NGINX_BASE/sbin/nginx -s reload
 fi
+
+delete_expire_cert_file
 
 # 测试证书是否生效
 # echo | openssl s_client -connect your.domain.com:443 | openssl x509 -noout -dates
